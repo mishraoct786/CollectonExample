d688bb2d2d5ad9cf1f9e59fbb5690b1cd75ed006
shubhamoct <mishra.oct786@gmail.com>
2018-03-10 15:21:42 +0530

    first commit

--- src/dequeinterface/mishra/org/TestArrayDeque.java
+++ src/dequeinterface/mishra/org/TestArrayDeque.java
@@ -1,5 +1,28 @@
 package dequeinterface.mishra.org;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class TestArrayDeque {
+	public static void main(String args [])
+	{
+		Deque<String> testArrayDeque = new ArrayDeque<String>();
+		
+		testArrayDeque.offer("trilokey");
+		testArrayDeque.add("shubham");
+		testArrayDeque.add("Rahul");
+		testArrayDeque.add("Shubham");
+		testArrayDeque.add("Tarun");
+		testArrayDeque.offer("jack");
+		testArrayDeque.offer("trilokey");
+		testArrayDeque.offer("meghana");
+		testArrayDeque.offerFirst("karan");
+		
+		for(String obj : testArrayDeque)
+		{
+		System.out.println(obj);	
+		}
+	}
+	
 
 }
